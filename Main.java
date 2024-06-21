11d11d0d6d26809096213d4e5b54a666065afa29
Jashanjot <jashanjotpanjrath@gmail.com>
2024-06-20 21:55:28 -0400

    Added more basic commands

--- Main.java
+++ Main.java
@@ -1,27 +1,47 @@
 // Uncomment this block to pass the first stage
 import java.util.Scanner;
 import java.util.HashSet;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.io.File;
 
 public class Main {
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BRIGHT_GREEN = "\u001B[92m";
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
+        String currentDirectory = System.getProperty("user.dir");
+        String user = System.getProperty("user.name");
+        String hostName = "unknown";
         
-        
+
+
+        try {
+            InetAddress addr = InetAddress.getLocalHost();
+            hostName = addr.getHostName();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
         Scanner scanner = new Scanner(System.in);
         HashSet <String> BuiltInCommands = new HashSet<String>();
         
         BuiltInCommands.add("echo");
         BuiltInCommands.add("exit");
         BuiltInCommands.add("type");
+        BuiltInCommands.add("clear");
+        BuiltInCommands.add("pwd");
+        BuiltInCommands.add("ls");
 
         String input;
 
         do {
 
-            System.out.print("$ ");
+            System.out.print(ANSI_BRIGHT_GREEN+user+"@"+hostName+ANSI_RESET+":"+ANSI_BLUE+ currentDirectory+ ANSI_RESET+"$ ");
             input = scanner.nextLine();
             String [] arrofstr = input.split(" ");
-            if(input.equals("exit 0")) {
+            if(arrofstr[0].equals("exit")) {
                 break;
             } else if(arrofstr[0].equals("echo")){
                  
@@ -42,11 +62,36 @@ public class Main {
                  } else{
                     System.out.println(arrofstr[1] + ": not found");
                  }
-            } else{
+            } else if(arrofstr[0].equals("clear")){
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            } else if(arrofstr[0].equals("pwd")){
+                System.out.println(currentDirectory);
+            } else if(arrofstr[0].equals("ls")){
+                File directory = new File(currentDirectory);
+                File[] files = directory.listFiles();
+
+                for(File file: files){
+                    String fileName = file.getName();
+                    if(!fileName.startsWith(".")){
+                        String toPrint;
+                        if(file.isDirectory()){
+                            toPrint = ANSI_BLUE + fileName + ANSI_RESET;
+                        } else{
+                            toPrint = ANSI_BRIGHT_GREEN+fileName +ANSI_RESET;
+                        }
+                        System.out.print( toPrint+"  ");
+                    }
+                }
+                System.out.println();
+            }
+            else{
                 System.out.println(input + ": command not found");
             }
             
             
         } while (true);
+
+        scanner.close();
     }
 }
